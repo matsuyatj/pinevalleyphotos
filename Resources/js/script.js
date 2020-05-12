1dc5cb42cf646298c84405bac5f055b59b71b318
matsuyatj <47612122+matsuyatj@users.noreply.github.com>
2020-05-11 23:08:31 -0500

    Add files via upload

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -1,62 +1,37 @@
 $(document).ready(function() {
 
-    // Slideshow
-    var slideIndex = 1;
-    showSlides(slideIndex);
-    var myTimer;
-
-    window.addEventListener("load",function() {
-        showSlides(slideIndex);
-        myTimer = setInterval(function(){plusSlides(1)}, 3500);
-
-    var slideshowContainer = document.getElementsByClassName('slideshow-inner')[0];
-    
-    slideshowContainer.addEventListener('mouseenter', pause)
-        
-    slideshowContainer.addEventListener('mouseleave', resume)
-    })
-    
-    function plusSlides(n){
-        clearInterval(myTimer);
-        if (n < 0){
-          showSlides(slideIndex -= n);
+    //sticky nav
+    $('.js--slideshow').waypoint(function(direction) {
+        if (direction == 'down') {
+            $('nav').addClass('sticky');
         } else {
-         showSlides(slideIndex += n); 
+            $('nav').removeClass('sticky');
         }
-    }
+    });
 
-    function currentSlide(n){
-        clearInterval(myTimer);
-        myTimer = setInterval(function(){plusSlides(n + 1)}, 3500);
-        showSlides(slideIndex = n);
-    }
-
-    function showSlides(n){
+    var slideIndex = 0;
+    showSlides();
+    
+    function showSlides() {
         var i;
-        var slides = document.getElementsByClassName("imageslides");
-        var dots = document.getElementsByClassName("thumbs");
-        if (n > slides.length) {slideIndex = 1}
-        if (n < 1) {slideIndex = slides.length}
+        var slides = document.getElementsByClassName('imageslides');
+        var dots = document.getElementsByClassName('thumbs');
         for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
+            slides[i].style.display = 'none';
         }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}
         for (i = 0; i < dots.length; i++) {
             dots[i].className = dots[i].className.replace(" active", "");
         }
-        slides[slideIndex-1].style.display = "block";
+        slides[slideIndex-1].style.display = 'block';
         dots[slideIndex-1].className += " active";
+        setTimeout(showSlides, 4000);
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
     }
    
-    pause = () => {
-        clearInterval(myTimer);
-    }
-
-    resume = () =>{
-        clearInterval(myTimer);
-        myTimer = setInterval(function(){plusSlides(slideIndex)}, 3500);
-    }
-    
-
 
 });
-
